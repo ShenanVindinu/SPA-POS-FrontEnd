54947d8f8e937ce7a3c2f3d8b23c4fa6cc4e3fe0
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-25 17:54:58 +0530

    when saved! all items will show on the table

--- assets/js/Item.js
+++ assets/js/Item.js
@@ -33,6 +33,37 @@ $(document).ready(function() {
             }
         });
 
+        $.ajax({
+            url: "http://localhost:8080/POS_BackEnd__JavaEE/Item",
+            type: "GET",
+            headers: {"Content-Type": "application/json"},
+            success: (res) => {
+                console.log(JSON.stringify(res));
+                populateItemTable(res);
+            },
+            error: (res) => {
+                console.error(res);
+            }            
+        });
+
+        function populateItemTable(items) {
+            const $tableBody = $('#ItemTableBody');
+            $tableBody.empty(); 
+        
+            items.forEach(item => {
+                const $row = $('<tr></tr>'); // Create a new row
+                
+                // Create and append cells to the row
+                $('<td></td>').text(item.id).appendTo($row);
+                $('<td></td>').text(item.name).appendTo($row);
+                $('<td></td>').text(item.price).appendTo($row);
+                $('<td></td>').text(item.qty).appendTo($row);
+        
+                // Append the row to the table body
+                $tableBody.append($row);
+            });
+        }
+
 
     });
 
