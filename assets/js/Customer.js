ea319790441a74b375702afd2372322708291d84
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-25 17:26:52 +0530

    when saved! all customers will show on the table

--- assets/js/Customer.js
+++ assets/js/Customer.js
@@ -33,6 +33,38 @@ $(document).ready(function () {
             }            
         });
 
+        $.ajax({
+            url: "http://localhost:8080/POS_BackEnd__JavaEE/Customer",
+            type: "GET",
+            headers: {"Content-Type": "application/json"},
+            success: (res) => {
+                console.log(JSON.stringify(res));
+                populateCustomerTable(res);
+            },
+            error: (res) => {
+                console.error(res);
+            }            
+        });
+
+        function populateCustomerTable(customers) {
+            const $tableBody = $('#customerTableBody');
+            $tableBody.empty(); 
+        
+            customers.forEach(customer => {
+                const $row = $('<tr></tr>'); // Create a new row
+                
+                // Create and append cells to the row
+                $('<td></td>').text(customer.id).appendTo($row);
+                $('<td></td>').text(customer.name).appendTo($row);
+                $('<td></td>').text(customer.address).appendTo($row);
+                $('<td></td>').text(customer.salary).appendTo($row);
+        
+                // Append the row to the table body
+                $tableBody.append($row);
+            });
+        }
+        
+
     });
 
 });
