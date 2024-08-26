e5900f74939eec6d68bbd1df1522b7f02be2feba
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-26 17:40:05 +0530

    customer delete sucessfully implemented

--- assets/js/Customer.js
+++ assets/js/Customer.js
@@ -47,12 +47,45 @@ $(document).ready(function () {
                 }            
             });
           }, 1000);
-        
+    });
 
-        function populateCustomerTable(customers) {
-            const $tableBody = $('#customerTableBody');
-            $tableBody.empty(); 
-        
+    $("#CustomerDeleteButton").click(function() {
+
+        let CusName = $("#CustomerNameField").val();
+    
+        $.ajax({
+            url: `http://localhost:8080/POS_BackEnd__JavaEE/Customer?name=${encodeURIComponent(CusName)}`,
+            type: "DELETE",
+            success: (res) => {
+                console.log("Delete Response:", JSON.stringify(res));
+                $.ajax({
+                    url: "http://localhost:8080/POS_BackEnd__JavaEE/Customer",
+                    type: "GET",
+                    headers: {"Content-Type": "application/json"},
+                    success: (res) => {
+                        console.log("Customer List:", JSON.stringify(res));
+                        populateCustomerTable(res);
+                    },
+                    error: (res) => {
+                        console.error("Error fetching customer list:", res);
+                    }            
+                });
+            },
+            error: (res) => {
+                console.log("Error during deletion:", JSON.stringify(res));
+            }
+        });
+    });
+    
+    function populateCustomerTable(customers) {
+        const $tableBody = $('#customerTableBody');
+        $tableBody.empty(); 
+    
+        if (customers.length === 0) {
+            const $row = $('<tr></tr>');
+            $('<td colspan="4">No customers available</td>').appendTo($row);
+            $tableBody.append($row);
+        } else {
             customers.forEach(customer => {
                 const $row = $('<tr></tr>'); // Create a new row
                 
@@ -66,8 +99,7 @@ $(document).ready(function () {
                 $tableBody.append($row);
             });
         }
-        
-
-    });
+    }
+    
 
 });
