380abed67daadc94cdea02cc5bc73d8719d912b6
ShenanVindinu <shenanvindinu@gmail.com>
2024-10-15 16:39:27 +0530

    current date get added when order page loads

--- assets/js/OrderPage.js
+++ assets/js/OrderPage.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
-    
+    setDate(); 
+
+    function setDate() {
+        var today = new Date().toISOString().split('T')[0];
+        $('#OrderDateField').val(today);
+    }
 
 });
